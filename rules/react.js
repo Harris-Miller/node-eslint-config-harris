57ce5c57562967a63d081f7b32895bf89fc87a9a
harris-miller <harrismillerconsulting@gmail.com>
2017-03-23 11:55:27 -0600

    added 'children' to prop-types ignore list

--- rules/react.js
+++ rules/react.js
@@ -183,7 +183,8 @@ module.exports = {
 
     // Prevent missing props validation in a React component definition
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
-    'react/prop-types': ['error', { ignore: ['dipatch'], customValidators: [], skipUndeclared: false }],
+    // I don't care what the docs say, 'children' gets ignored, also 'dispatch' because of react-redux
+    'react/prop-types': ['error', { ignore: ['children', 'dipatch'], customValidators: [], skipUndeclared: false }],
 
     // Prevent missing React when using JSX
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md

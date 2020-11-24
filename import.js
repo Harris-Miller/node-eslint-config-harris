1b7bf20077b6d5bba0877466dab0e805f8d86994
hmiller <hmiller@alteryx.com>
2020-11-23 18:39:32 -0700

    removing old everything

--- import.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-module.exports = {
-  extends: [
-    path.join(__dirname, './rules/import')
-  ].map(require.resolve)
-};
--- typescript.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-module.exports = {
-  extends: [
-    path.join(__dirname, './rules/typescript')
-  ].map(require.resolve)
-};
